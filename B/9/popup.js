cdcbe43510ece3fdaf1714f3ead5d13f97481117
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2017-12-31 20:01:36 +0530

    B9 updated, done!

--- B/9/popup.js
+++ B/9/popup.js
@@ -1,12 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-    if(url.startsWith("https://bits-oasis.org") || url.startsWith("https://bits-apogee.org") || url.startsWith("https://bits-bosm.org"))
-    {
-      chrome.tabs.executeScript({
-        code: 'console.log("Hurray!")'
-      });  
-    }
-  });
-});
+console.log("Hurray!");
